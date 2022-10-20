84138ac9699e27c1c3805bbdf628c198290a2858
DMV22 <maksdivnich12@gmail.com>
2022-10-20 21:13:54 +0300

    create: add validation forms

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
   const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "coverflow",
-  slidesPerView: 1,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: "coverflow",
+    slidesPerView: 1,
   });
 
   const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-  effect: "coverflow",
-  slidesPerView: 1,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    effect: "coverflow",
+    slidesPerView: 1,
   });
 
   let menuButton = $('.menu-button')
@@ -50,5 +50,24 @@ $(document).ready(function () {
     modalDialog.removeClass("modal__dialog--visible")
   }
 
+  // Опрацювання форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please specify your telephone",
+        },
+      },
+    });
+  });
 
+  $("[name=phone]").mask("+38(999)-999-9999");
 });
